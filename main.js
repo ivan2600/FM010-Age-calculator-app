ea1e2ce44d986fa9e0e1afceadf8d1c4c9460ca6
Ivan Adamo <ivan.adamo.8@gmail.com>
2024-01-06 17:35:33 -0300

    finished

--- main.js
+++ main.js
@@ -17,8 +17,9 @@ let fechaInput;
 let diferencia;
 let milisegundosEnDia = 1000 * 60 * 60 * 24;
 
-
+//Escuchando el evento click en SUBMIT
 submit.addEventListener('click', ()=>{
+  //Borrando todas las alertas de error
   pDay.classList.remove('active');
   pMonth.classList.remove('active');
   pYear.classList.remove('active');
@@ -28,8 +29,9 @@ submit.addEventListener('click', ()=>{
   day.classList.remove('active');
   month.classList.remove('active');
   year.classList.remove('active');
-
+  //SI el valor de "dia" es menor a 1 o mayor a 31
   if (day.value < 1 || day.value > 31) {
+    //Activar alertas de error
     pDay.classList.add('active');
     errorDay.style.display = "block";
     day.classList.add('active');
@@ -40,9 +42,10 @@ submit.addEventListener('click', ()=>{
     return
   } else {
     calcularFecha()
-    
   }
+  //Si el mes es menor a 1 o mayor a 12
   if (month.value < 1 || month.value > 12) {
+    //activar las alertas de error
     pMonth.classList.add('active');
     errorMonth.style.display = "block";
     month.classList.add('active');
@@ -54,7 +57,9 @@ submit.addEventListener('click', ()=>{
   } else {
     calcularFecha()
   }
+  //SI el año es menor al año actual
   if (year.value > fechaActual.getFullYear()) {
+    //Activar alertas de error
     pYear.classList.add('active');
     errorYear.style.display = "block";
     year.classList.add('active');
@@ -67,24 +72,25 @@ submit.addEventListener('click', ()=>{
   }
 
   function calcularFecha() {
+    //Convertimos los valores de los INPUTS a una fecha y la asignamos a "fechaInput"
     fechaInput = new Date(year.value, month.value - 1, day.value);
-  console.log(fechaInput);
-  
-  diferencia = fechaActual.getTime() - fechaInput.getTime();
-  
-  let dias = Math.floor(diferencia/milisegundosEnDia);
-  let años = Math.floor(dias / 365);
-  console.log('años: ' + años);
-  outYears.innerHTML = años;
-  let meses = Math.floor(dias/ 30.41);
-  let mesesRestantes = Math.floor(meses - (años * 12));
-  console.log('meses: ' + mesesRestantes);
-  outMonths.innerHTML = mesesRestantes;
-  let diasRestantes = Math.floor(dias - (meses * 30.41))
-  console.log('dias: ' + diasRestantes);
-  outDays.innerHTML = diasRestantes;
+    //Encontramos la diferencia en milisegundos entre "fechaActual" y "fechaInput"
+    diferencia = fechaActual.getTime() - fechaInput.getTime();
+    //Obtenemos cuantos dias son los milisegundos en "diferencia"
+    let dias = Math.floor(diferencia/milisegundosEnDia);
+    //Obtenemos cuantos años hay en "diferencia"
+    let años = Math.floor(dias / 365);
+    //imprimir cantidad de años
+    outYears.innerHTML = años;
+    //Obtenemos cuantos meses hay en diferencia
+    let meses = Math.floor(dias/ 30.41);
+    //Le restamos a meses la cantidad de meses que entan englobados en "años"
+    let mesesRestantes = Math.floor(meses - (años * 12));
+    //Imprimimos cantidad de meses
+    outMonths.innerHTML = mesesRestantes;
+    //Obtenemos la catidad de dias descontando los dias ocupados por "meses"
+    let diasRestantes = Math.floor(dias - (meses * 30.41))
+    //Imprimimos cantidd de dias
+    outDays.innerHTML = diasRestantes;
   }
-  
 })
-
-//
